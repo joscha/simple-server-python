a151e3537d9d8125b166be3b8285e0b21f1dbbcc
Joscha Feth <joscha@feth.com>
2018-11-25 00:40:51 +1100

    .

--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
-from flask import Flask
 from RPLCD.i2c import CharLCD
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 if __name__ == '__main__':
-    lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-              cols=20, rows=4, dotsize=8,
+    lcd = CharLCD(i2c_expander='PCF8574', address=0x3f, port=1,
+              cols=16, rows=2, dotsize=8,
               charmap='A02',
               auto_linebreaks=True,
               backlight_enabled=True)
+    lcd.clear()
+    lcd.write_string('Hello world')
+    lcd.close()
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# Enable I2C
+modprobe i2c-dev
+
+# sudo i2cdetect -y 1
+
+python -u src/main.py
