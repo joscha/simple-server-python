71fb4c6808b1ac61849b05c159388679e35af863
Joscha Feth <joscha@feth.com>
2018-11-28 20:16:13 +1100

    .

--- src/main.py
+++ src/main.py
@@ -9,14 +9,17 @@ if __name__ == '__main__':
               charmap='A02',
               auto_linebreaks=True,
               backlight_enabled=True)
-    #lcd.write_string('Hello world')
+    lcd.write_string('Hello world')
     #lcd.close()
 
     SOLAREDGE_API_KEY = os.environ['SOLAREDGE_API_KEY']
     SOLAREDGE_SITE_ID = os.environ['SOLAREDGE_SITE_ID']
 
     s = solaredge.Solaredge(SOLAREDGE_API_KEY)
-    print(s.get_current_power_flow(SOLAREDGE_SITE_ID))
-
-    lcd.write(s.siteCurrentPowerFlow)
+    currentPowerFlow = s.get_current_power_flow(SOLAREDGE_SITE_ID)
+    print(currentPowerFlow)
+    p = currentPowerFlow["siteCurrentPowerFlow"]["GRID"]["currentPower"]
+    print(p)
+    lcd.clear()
+    lcd.write_string(str(p))
     lcd.close()
